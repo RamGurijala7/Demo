98ad3833825e92c38964b49b7850d93d747e1750
User <User@DESKTOP-H40ORDH>
2022-06-08 11:36:53 +0530

    first commit

---
+++ src/main/java/com/maven/uplod/MavenUploadDemo/MavenUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.maven.uplod.MavenUploadDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenUploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenUploadDemoApplication.class, args);
+	}
+
+}
